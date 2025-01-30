2c7115fc7de62ce5c7232fb1289a7c9aff9036bb
StefanSirkovic <stefansirkovic@gmail.com>
2025-01-30 20:10:02 +0100

    Fix '.env' file path

--- backend/notification-service/src/main/java/com/team/project/manager/NotificationServiceApplication.java
+++ backend/notification-service/src/main/java/com/team/project/manager/NotificationServiceApplication.java
@@ -7,7 +7,7 @@ import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class NotificationServiceApplication {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().directory("backend").load();
+        Dotenv dotenv = Dotenv.configure().directory("backend").ignoreIfMissing().load();
         System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
 
         SpringApplication.run(NotificationServiceApplication.class, args);
